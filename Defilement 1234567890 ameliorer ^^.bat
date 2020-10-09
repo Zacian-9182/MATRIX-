70047963a2252ea1e131b4b9ff271013f15758bf
Zacian-9182 <62335081+Zacian-9182@users.noreply.github.com>
2020-10-09 19:29:04 +0200

    Add files via upload

---
+++ Defilement 1234567890 ameliorer ^^.bat
@@ -0,0 +1,15 @@
+@echo off
+title Matrix
+color a
+mode 1000
+goto Greeting
+
+:Greeting
+cls
+echo Salut les gens vous pensez quoi de mon code ?
+pause
+cls
+goto Matrix
+:Matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto Matrix
